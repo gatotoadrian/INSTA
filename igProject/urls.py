19967366adf7c859eb0d28f73071b4ce6b0170d6
gatotoadrian <gatotoadrian@gmail.com>
2022-06-09 05:03:58 +0300

    imported  urls paths

--- igProject/urls.py
+++ igProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 import instagram
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
 from django.urls.conf import include
 from django_registration.backends.one_step.views import RegistrationView
 

8351c8698f2d1263f8d5913e67a143401e88f072
gatotoadrian <gatotoadrian@gmail.com>
2022-06-09 05:01:10 +0300

    linked instagtam  app on settings lpy

--- igProject/settings.py
+++ igProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'instagram',
+    'instagram', 
     'bootstrap5',
     'django_registration',
 ]

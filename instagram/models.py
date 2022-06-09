c013f6218c0af8d6a96156e2484eb920f45a2079
gatotoadrian <gatotoadrian@gmail.com>
2022-06-09 05:09:21 +0300

    created the Images   clas

--- instagram/models.py
+++ instagram/models.py
@@ -22,7 +22,7 @@ class Following(models.Model) :
     def __str__(self):
         return f'{self.username}'
         
-class Image(models.Model):
+class Image( models.Model):
     image=models.ImageField(upload_to='images')
     name=models.CharField(max_length=30)
     caption=models.CharField(max_length=30)
